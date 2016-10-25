2f84a8a53c5f47b399875f31743905ba864a6c6e
marcel <marcel.nicolay@gmail.com>
2016-10-25 13:36:58 -0200

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='django-webodt',
-    version='0.3.4',
+    version='0.3.5',
     author='NetAngels',
     author_email='info@netangels.ru',
     packages=['webodt', 'webodt.converters', 'webodt.tests'],
